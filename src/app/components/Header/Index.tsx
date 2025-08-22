2a2e773195e041cd7fe4f7fbcd599c90a84df0ba
David Silva <davidlsilva.sp@gmail.com>
2025-08-22 18:54:42 -0300

    feat: adicionar estilos e interações ao cabeçalho, formulário de contato e banner principal

--- src/app/components/Header/Index.tsx
+++ src/app/components/Header/Index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
 
--- src/app/components/MainBanner/Index.tsx
+++ src/app/components/MainBanner/Index.tsx
@@ -1,21 +1,35 @@
 import React from 'react'
-import styles from './MainBanner.module.css'
+import styles from './MainBanner.module.scss'
 import ContactForm from '../ContactForm/Index'
+import Image from 'next/image'
 
 
 const MainBanner = () => {
   return (
-         <div style={{ backgroundImage: `url("/main_banner.png")` }} className={styles.banner}>
-        <div className={styles.bannerContent}>
-          <h2 className={styles.bannerText}>
-            Atraia<br /> <span>Novos Clientes</span> <br />como nunca <br />antes
-          </h2>
-          <p className={styles.bannerSubtext}>
-            Transforme visitas em oportunidades reais. Cresça seu negócio com estratégias que geram resultados.
-          </p>
-          <ContactForm />
+        <div
+          style={{ backgroundImage: `url("/main_banner.png")` }}
+          className={styles.banner}
+        >
+          <Image
+            src="/main_banner.png"
+            alt=""
+            style={{ display: 'none' }}
+            unoptimized
+            width={0}
+            height={0}
+            fetchPriority="high"
+          />
+          
+          <div className={styles.bannerContent}>
+            <h2 className={styles.bannerText}>
+          Atraia<br /> <span>Novos Clientes</span> <br />como nunca <br />antes
+            </h2>
+            <p className={styles.bannerSubtext}>
+          Transforme visitas em oportunidades reais. Cresça seu negócio com estratégias que geram resultados.
+            </p>
+            <ContactForm />
+          </div>
         </div>
-      </div>
   )
 }
 
